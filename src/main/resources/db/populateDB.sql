89c0b8294305237212e112831af03897a4505a45
apushkin <apushkin@ipmks.com>
2018-10-22 16:40:17 +0300

    Change db scripts. Add meals table

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -9,3 +10,8 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (meal_id, user_id, description, calories, date_time) VALUES
+(100003, 100000, 'Завтрак', 500, '2015-05-30 10:00:00.879468'),
+(100004, 100000, 'Обед', 1000, '2015-05-30 13:00:00.438532'),
+(100005, 100000, 'Ужин', 500, '2015-05-30 20:00:00.332587');
