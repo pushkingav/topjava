26f31b92d9ec1011f8a1ca53f89f5f9631f8732a
apushkin <apushkin@ipmks.com>
2018-10-25 10:27:10 +0300

    Add tests.

---
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -0,0 +1,22 @@
+package ru.javawebinar.topjava;
+
+import ru.javawebinar.topjava.model.Meal;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MealTestData {
+
+    public static void assertMatch(Meal actual, Meal expected) {
+        assertThat(actual).isEqualToIgnoringGivenFields(expected, "id");
+    }
+
+    public static void assertMatch(Iterable<Meal> actual, Meal... expected) {
+        assertMatch(actual, Arrays.asList(expected));
+    }
+
+    public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
+        assertThat(actual).usingElementComparatorIgnoringFields("id").isEqualTo(expected);
+    }
+}
---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,59 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.web.SecurityUtil;
+
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.*;
+@ContextConfiguration({
+     "classpath:spring/spring-app.xml",
+     "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+public class MealServiceTest {
+    @Autowired
+    private MealService service;
+
+    @Test
+    public void get() {
+        Meal meal = new Meal(LocalDateTime.now(), "testMeal", 500);
+        Meal created = service.create(meal, 100002);
+        MealTestData.assertMatch(meal, created);
+
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getBetweenDates() {
+    }
+
+    @Test
+    public void getBetweenDateTimes() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void create() {
+    }
+}
